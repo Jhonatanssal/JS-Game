ee8db2b95ad605ef56c56c91d9ca26aa8801dbef
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-19 01:37:20 -0500

    Add scenes and styles

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
+import '../styles/style.css';
 import config from './Config/config';
-import BootScene from "./Scenes/BootScene";
-import CreditsScene from "./Scenes/CreditsScene";
+import BootScene from './Scenes/BootScene';
+import CreditsScene from './Scenes/CreditsScene';
+import GameOverScene from './Scenes/GameOverScene';
 import GameScene from './Scenes/GameScene';
-import LeaderBoardScene from "./Scenes/LeaderBoardScene";
-import InstructionsScene from "./Scenes/OptionsScene";
-import PreloaderScene from "./Scenes/PreloaderScene";
-import TitleScene from "./Scenes/TitleScene";
+import LeaderBoardScene from './Scenes/LeaderBoardScene';
+import InstructionsScene from './Scenes/OptionsScene';
+import PreloaderScene from './Scenes/PreloaderScene';
+import TitleScene from './Scenes/TitleScene';
 
 class Game extends Phaser.Game {
-  constructor () {
+  constructor() {
     super(config);
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);
     this.scene.add('Instructions', InstructionsScene);
-    this.scene.add('LeaderBoard', LeaderBoardScene)
+    this.scene.add('LeaderBoard', LeaderBoardScene);
     this.scene.add('Credits', CreditsScene);
     this.scene.add('Game', GameScene);
-    this.scene.start('LeaderBoard');
+    this.scene.add('GameOver', GameOverScene);
+    this.scene.start('Boot');
   }
 }
 
