9e43b6afe6f001fdfe05d053247c8beeef461dc6
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-19 01:42:20 -0500

    Add preloader scene

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -1,86 +1,97 @@
-import 'phaser';
+/* eslint-disable radix */
+import Phaser from 'phaser';
 import playbtn from '../assets/playbtn.png';
 import creditsbtn from '../assets/creditsbtn.png';
 import howtoplaybtn from '../assets/howtoplaybtn.png';
+import sky from '../assets/sky.png';
+import ground from '../assets/platform.png';
+import star from '../assets/star.png';
+import bomb from '../assets/bomb.png';
+import dude from '../assets/dude.png';
 
 export default class PreloaderScene extends Phaser.Scene {
-  constructor () {
+  constructor() {
     super('Preloader');
   }
 
-  init () {
+  init() {
     this.readyCount = 0;
   }
 
-  ready () {
+  ready() {
     this.readyCount += 1;
-    if (this.readyCount === 2) {
+    if (this.readyCount === 1) {
       this.scene.start('Title');
     }
   }
 
-  preload () {
-    this.add.image(400, 200, 'logo');
+  preload() {
+    // load images
+    this.load.image('sky', sky);
+    this.load.image('ground', ground);
+    this.load.image('star', star);
+    this.load.image('bomb', bomb);
+    this.load.spritesheet('dude', dude, { frameWidth: 32, frameHeight: 48 });
 
-    let progressBar = this.add.graphics();
-    let progressBox = this.add.graphics();
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
     progressBox.fillStyle(0x222222, 0.8);
     progressBox.fillRect(240, 270, 320, 50);
 
-    let width = this.cameras.main.width;
-    let height = this.cameras.main.height;
-    let loadingText = this.make.text({
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const loadingText = this.make.text({
       x: width / 2,
-      y: height /2 - 50,
+      y: height / 2 - 50,
       text: 'Loading...',
       style: {
         font: '20px monospace',
-        fill: '#ffffff'
-      }
+        fill: '#ffffff',
+      },
     });
     loadingText.setOrigin(0.5, 0.5);
 
-    let percentText = this.make.text({
+    const percentText = this.make.text({
       x: width / 2,
       y: height / 2 - 5,
       text: '0%',
       style: {
         font: '18px monospace',
-        fill: '#ffffff'
-      }
+        fill: '#ffffff',
+      },
     });
     percentText.setOrigin(0.5, 0.5);
 
-    let assetText = this.make.text({
+    const assetText = this.make.text({
       x: width / 2,
       y: height / 2 + 50,
       text: '',
       style: {
         font: '18px monospace',
-        fill: '#ffffff'
-      }
+        fill: '#ffffff',
+      },
     });
     assetText.setOrigin(0.5, 0.5);
 
-    this.load.on('progress', function (value) {
-      percentText.setText(parseInt(value * 100) + '%');
+    this.load.on('progress', (value) => {
+      percentText.setText(`${parseInt(value * 100)}%`);
       progressBar.clear();
       progressBar.fillStyle(0xffffff, 1);
       progressBar.fillRect(250, 280, 300 * value, 30);
     });
 
-    this.load.on('fileprogress', function (file) {
-      assetText.setText('Loading asset: ' + file.key);
+    this.load.on('fileprogress', (file) => {
+      assetText.setText(`Loading asset: ${file.key}`);
     });
 
-    this.load.on('complete', function () {
+    this.load.on('complete', () => {
       progressBar.destroy();
       progressBox.destroy();
       loadingText.destroy();
       percentText.destroy();
       assetText.destroy();
       this.ready();
-    }.bind(this));
+    });
 
     this.timeEvent = this.time.delayedCall(3000, this.ready, [], this);
 
@@ -88,6 +99,4 @@ export default class PreloaderScene extends Phaser.Scene {
     this.load.image('howtoplaybtn', howtoplaybtn);
     this.load.image('creditsbtn', creditsbtn);
   }
-
-  create () {}
-};
+}
