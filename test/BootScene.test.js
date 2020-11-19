8fd90452a8170bbfde433610b6fa27bbbd273eb1
jssarrazolaa <jssarrazolaa@unal.edu.co>
2020-11-19 13:47:20 -0500

    Revert

--- test/BootScene.test.js
+++
@@ -1,8 +0,0 @@
-import Phaser from 'phaser';
-import BootScene from '../src/Scenes/BootScene';
-
-describe('It test the existing subclass', () => {
-  it('has to be a subclass of Phaser.Scene', () => {
-    expect(BootScene).toBeSubclassOf(Phaser.Scene);
-  });
-});
--- test/LeaderBoardscene.test.js
+++
@@ -1,8 +0,0 @@
-import Phaser from 'phaser';
-import LeaderBoardScene from '../src/Scenes/LeaderBoardScene';
-
-describe('It test the existing subclass', () => {
-  it('has to be a subclass of Phaser.Scene', () => {
-    expect(LeaderBoardScene).toBeSubclassOf(Phaser.Scene);
-  });
-});
